9c51e03d6182e022e6cb3547fca7b47893de7e15
Naoki Morita <namorit@gmail.com>
2016-03-20 05:18:01 +0900

    Initial commit

---
+++ src/Apps/Apps.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Apps() {
+  return (
+    <div className="apps">
+    <p>apps</p>
+    </div>
+  );
+}
---
+++ src/Index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './Header';
+import Apps from './Apps/Apps';
+import Footer from './Footer';
+
+export default function Index() {
+  return (
+    <div>
+      <Header />
+      <Apps />
+      <Footer />
+    </div>
+  );
+}
