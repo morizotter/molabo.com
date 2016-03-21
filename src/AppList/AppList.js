56ab008a38503df7e7ed6adf88c77f8e1fb0747b
Naoki Morita <namorit@gmail.com>
2016-03-21 17:01:52 +0900

    Show list

--- src/AppList/AppList.js
+++ src/AppList/AppList.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import App from './App';
+
+const apps = [
+  {
+    key: '0',
+    info: {
+      name: 'app a',
+    },
+  },
+  {
+    key: '1',
+    info: {
+      name: 'app b',
+    },
+  },
+  {
+    key: '2',
+    info: {
+      name: 'app c',
+    },
+  },
+];
 
 export default function AppList() {
+  const appNodes = apps.map((app) =>
+    <App name={app.info.name} key={app.key} />
+  );
+
+  apps.forEach((app) => {
+    console.log(app);
+  });
+
   return (
     <div className="apps">
-    <p>apps</p>
+      { appNodes }
     </div>
   );
 }
