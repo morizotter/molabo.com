acbd971a7f15f68b42f45ad9ec2e0f0733fc1d16
Naoki Morita <namorit@gmail.com>
2016-03-26 20:36:13 +0900

    Add bootstrap

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Main from './Components/Main/Main';
+require('../node_modules/bootstrap/dist/css/bootstrap.css');
 
 function App() {
   return <Main />;
--- webpack.config.js
+++ webpack.config.js
@@ -7,21 +7,21 @@ module.exports = {
   module: {
     loaders: [
       {
-        test: /\.js$/,
+        test: /\.js[x]?$/,
         exclude: /node_modules/,
         loader: 'babel',
         query: {
           presets: ['react', 'es2015'],
         },
       },
+      { test: /\.css$/, loader: 'style-loader!css-loader' },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file' },
+      { test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000' },
       {
-        test: /\.jsx$/,
-        exclude: /node_modules/,
-        loader: 'babel',
-        query: {
-          presets: ['react', 'es2015'],
-        },
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url?limit=10000&mimetype=application/octet-stream',
       },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml' },
     ],
   },
   resolve: {
